c486e270b5091e7c1c87cda215162098b9f32b13
Peter Illes <peter.illes@gofurther.digital>
2021-07-16 14:35:43 +0200

    Console Kernel bugfix.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -10,32 +10,27 @@ class Kernel extends ConsoleKernel
     /**
      * The Artisan commands provided by your application.
      *
-     * @var array
+     * @var array<string>
      */
     protected $commands = [
         //
     ];
 
     /**
-     * Define the application's command schedule.
-     *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
-     * @return void
-     */
-    protected function schedule(Schedule $schedule)
-    {
-        // $schedule->command('inspire')->hourly();
-    }
-
-    /**
      * Register the commands for the application.
-     *
-     * @return void
      */
-    protected function commands()
+    protected function commands(): void
     {
         $this->load(__DIR__.'/Commands');
 
-        require base_path('routes/console.php');
+//        require base_path('routes/console.php');
+    }
+
+    /**
+     * Define the application's command schedule.
+     */
+    protected function schedule(Schedule $schedule): void
+    {
+        //
     }
 }
